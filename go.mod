58254d7e7db51b77ee7e43eb5f6b090d02c5e871
Arpit Blagan <blaganarpit@gmail.com>
2025-01-01 19:47:16 +0530

    auth using jwt

--- go.mod
+++ go.mod
@@ -3,6 +3,8 @@ module go-ws
 go 1.23.3
 
 require (
+	github.com/golang-jwt/jwt v3.2.2+incompatible
+	github.com/golang-jwt/jwt/v5 v5.2.1 // indirect
 	github.com/gorilla/mux v1.8.1 // indirect
 	github.com/gorilla/websocket v1.5.3 // indirect
 )
--- main.go
+++ main.go
@@ -1,12 +1,27 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
+	"time"
 
+	"github.com/golang-jwt/jwt/v5"
 	"github.com/gorilla/mux"
 	"github.com/gorilla/websocket"
 )
+
+type Info struct{
+	Email string `json:"email"`
+	Password string `json:"password"`
+}
+
+type Claims struct {
+	Username string `json:"username"`
+	jwt.RegisteredClaims
+}
+
+var jwtKey = []byte("my_secret_key")
 
 //Upgrader function
 var upgrader = websocket.Upgrader{
@@ -44,15 +59,86 @@ func handleWebSocket(w http.ResponseWriter,r *http.Request){
 	}
 }
 
+func handleLogin(w http.ResponseWriter ,r *http.Request){
+	var info Info
+	if err:=json.NewDecoder(r.Body).Decode(&info);err!=nil{
+		fmt.Println("Error while decoding the request body")
+		http.Error(w,"Error while decoding :(",http.StatusInternalServerError)
+		return
+	}
+	// check in DB if the user exits let's say in our case it exits everytime.
+
+	expirationTime := time.Now().Add(15 * time.Minute)
+	claims := &Claims{
+		Username: info.Email,
+		RegisteredClaims: jwt.RegisteredClaims{
+			ExpiresAt: jwt.NewNumericDate(expirationTime),
+		},
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	
+	tokenString, err := token.SignedString(jwtKey)
+	if err != nil {
+		http.Error(w, "Internal server error", http.StatusInternalServerError)
+		return
+	}
+
+	http.SetCookie(w, &http.Cookie{
+		Name:     "token",
+		Value:    tokenString,
+		Expires:  expirationTime,
+		HttpOnly: true,
+	})
+}
+
+
+func checkJwt(w http.ResponseWriter, r *http.Request) {
+	cookie, err := r.Cookie("token")
+	if err != nil {
+		if err == http.ErrNoCookie {
+			http.Error(w, "Unauthorized", http.StatusUnauthorized)
+			return
+		}
+		http.Error(w, "Bad request", http.StatusBadRequest)
+		return
+	}
+
+	tokenStr := cookie.Value
+	claims := &Claims{}
+
+	token, err := jwt.ParseWithClaims(tokenStr, claims, func(token *jwt.Token) (interface{}, error) {
+		return jwtKey, nil
+	})
+	if err != nil {
+		if err == jwt.ErrSignatureInvalid {
+			http.Error(w, "Unauthorized", http.StatusUnauthorized)
+			return
+		}
+		http.Error(w, "Bad request", http.StatusBadRequest)
+		return
+	}
+
+	if !token.Valid {
+		http.Error(w, "Unauthorized", http.StatusUnauthorized)
+		return
+	}
+
+	w.Write([]byte(fmt.Sprintf("Welcome, %s!", claims.Username)))
+}
+
+
 func main(){
 	fmt.Println("Hey from the Go server")
 	// Here we can have our DB connection....
-	
+
 	router:=mux.NewRouter()
-	router.HandleFunc("/ws",handleWebSocket).Methods("GET")
-	err:=http.ListenAndServe(":9000",router)
+	// router.HandleFunc("/ws",handleWebSocket).Methods("GET")
+	router.HandleFunc("/login",handleLogin).Methods("POST")
+	router.HandleFunc("/check",checkJwt).Methods("GET")
+	err:=http.ListenAndServe(":8000",router)
 	if(err!=nil){
-		fmt.Println("Error while creating a server :(")
+		fmt.Println("Error while creating a server :(",err)
 		return
 	}
 	fmt.Println("Server is running on 9000 port :)")
